f6e93ceb6f2c71d060c852abe08d987d97410658
Markus Reinhold <markusreinhold@icloud.com>
2017-09-20 22:45:33 +0200

    Little performance optimization

--- src/HasFriendClasses.php
+++ src/HasFriendClasses.php
@@ -6,18 +6,25 @@ trait HasFriendClasses
 {
     public function __call($name, $arguments)
     {
-        return $this->executeIfTheCallerIsFriend(function () use ($name, $arguments) {
-            $this->$name(...$arguments);
-        }, function ($context) use ($name) {
-            throw new FriendException(
-                sprintf(
-                    'Cannot access method %s::%s() from context \'%s\'',
-                    get_class($this),
-                    $name,
-                    $context
-                )
-            );
-        });
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+        $context = '';
+
+        if (count($trace) > 1) {
+            $callerClass = $context = $trace[1]['class'];
+
+            if (in_array($callerClass, self::FRIEND_CLASSES)) {
+                return $this->$name(...$arguments);
+            }
+        }
+
+        throw new FriendException(
+            sprintf(
+                'Cannot access method %s::%s() from context \'%s\'',
+                get_class($this),
+                $name,
+                $context
+            )
+        );
     }
 
     public static function __callStatic($name, $arguments)
@@ -25,7 +32,6 @@ trait HasFriendClasses
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
         $context = '';
 
-        // The information is stored in the third index.
         if (count($trace) > 1) {
             $callerClass = $context = $trace[1]['class'];
 
@@ -46,50 +52,48 @@ trait HasFriendClasses
 
     public function __get($name)
     {
-        return $this->executeIfTheCallerIsFriend(function () use ($name) {
-            return $this->$name;
-        }, function ($context) use ($name) {
-            throw new FriendException(
-                sprintf(
-                    'Cannot access property %s::$%s from context \'%s\'',
-                    get_class($this),
-                    $name,
-                    $context
-                )
-            );
-        });
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+        $context = '';
+
+        if (count($trace) > 1) {
+            $callerClass = $context = $trace[1]['class'];
+
+            if (in_array($callerClass, self::FRIEND_CLASSES)) {
+                return $this->$name;
+            }
+        }
+
+        throw new FriendException(
+            sprintf(
+                'Cannot access property %s::$%s from context \'%s\'',
+                get_class($this),
+                $name,
+                $context
+            )
+        );
     }
 
     public function __set($name, $value)
     {
-        $this->executeIfTheCallerIsFriend(function () use ($name, $value) {
-            $this->$name = $value;
-        }, function ($context) use ($name) {
-            throw new FriendException(
-                sprintf(
-                    'Cannot access property %s::$%s from context \'%s\'',
-                    get_class($this),
-                    $name,
-                    $context
-                )
-            );
-        });
-    }
-
-    private function executeIfTheCallerIsFriend(callable $if, callable $else)
-    {
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3);
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
         $context = '';
 
-        // The information is stored in the third index.
-        if (count($trace) > 2) {
-            $callerClass = $context = $trace[2]['class'];
+        if (count($trace) > 1) {
+            $callerClass = $context = $trace[1]['class'];
 
             if (in_array($callerClass, self::FRIEND_CLASSES)) {
-                return $if();
+                $this->$name = $value;
+                return;
             }
         }
 
-        $else($context);
+        throw new FriendException(
+            sprintf(
+                'Cannot access property %s::$%s from context \'%s\'',
+                get_class($this),
+                $name,
+                $context
+            )
+        );
     }
 }
